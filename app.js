2945317f13ae8182a4bf770b90b30efc7d4e915d
marselle13 <98524205+marselle13@users.noreply.github.com>
2022-07-10 01:35:03 +0400

    finish all page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+
 // Get started button
 $(".start-btn").on("click", function () {
   location.href = "create.html";
@@ -29,14 +30,16 @@ const checker4 = document.querySelector(".check4");
 const error1 = document.querySelector(".error1-box");
 const error2 = document.querySelector(".error2-box");
 const error3 = document.querySelector(".error3-box");
+const error4 = document.querySelector(".error4-box");
 
 let regExp = /@redberry\.ge/;
 
-$(".next-btn").on("click", function (e) {
+$("#submit").on("click", function (e) {
   e.preventDefault();
   check1();
   check2();
   check3();
+  check4();
 
   if (
     checker1.style.display === "block" &&
@@ -45,6 +48,7 @@ $(".next-btn").on("click", function (e) {
   ) {
     $(".box-num1").text("");
     $(".box-num1").addClass("done");
+    location.href = "exp.html";
   }
 });
 
@@ -57,6 +61,7 @@ function check1() {
   } else {
     checker1.style.display = "none";
     error1.style.display = "block";
+
     $("#name").css("background", "#Ffefef");
   }
   if (names.value === "") {
@@ -85,6 +90,7 @@ function check2() {
 }
 
 function check3() {
+  // phone checker
   if (phone.value.length === 9) {
     checker3.style.display = "block";
     error3.style.display = "none";
@@ -101,18 +107,26 @@ function check3() {
   }
 }
 
-// invalid data close
+function check4() {
+  if (date.getAttribute("required") === "") {
+    checker4.style.display = "block";
+    error4.style.display = "none";
+  }
+}
 
 $(".close1").on("click", function () {
   error1.style.display = "none";
+  $("#name").css("background", "#Fff");
 });
 
 $(".close2").on("click", function () {
   error2.style.display = "none";
+  $("#email").css("background", "#Fff");
 });
 
 $(".close3").on("click", function () {
   error3.style.display = "none";
+  $("#phone").css("background", "#Fff");
 });
 
 // function formatPhoneNumber(value) {
@@ -135,22 +149,73 @@ $(".close3").on("click", function () {
 //   inputField.value = formattedInputValue;
 // }
 
+//level of knowledge dropdown
 function show(anything) {
   document.querySelector(".textBox").value = anything;
 }
 
+//choose your character dropdown
+function show2(anything) {
+  document.querySelector(".textBox2").value = anything;
+}
+// dropdown open
 let dropdown = document.querySelector(".dropdown");
 dropdown.onclick = function () {
   dropdown.classList.toggle("active");
+  $(".box-2").addClass("reg");
+};
+//dropdown open
+let dropdown2 = document.querySelector(".dropdown2");
+dropdown2.onclick = function () {
+  dropdown2.classList.toggle("active");
+  $(".box-2").addClass("reg");
 };
 
-// var staticUrl = "https://chess-tournament-api.devtest.ge/api/grandmasters";
+// Json data
+const staticUrl = "https://chess-tournament-api.devtest.ge/api/grandmasters";
+const option = document.getElementById("option");
+fetch(staticUrl)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    let output = "";
 
-// $(document).ready(function () {
-//   function load_json_data(id, parent_id) {
-//     var html_code = "";
-//     $.getJSON(staticUrl, function (data) {
-//       html_code += $.each(data, function (key, value) {});
-//     });
-//   }
-// });
+    data.forEach((player) => {
+      output += `<div onclick="show2('${player.name}')" class="player"><div class="player-name">${player.name}</div><img   class="player-img" src=
+      "https://chess-tournament-api.devtest.ge${player.image}" /></div>`;
+    });
+    option.innerHTML = output;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+$(".done-btn").on("click", function () {
+  location.href = "done.html";
+});
+
+//save data
+$("#form").addEventListener("submit", function (e) {
+  e.prevenetDefault();
+  const formData = new FormData(this);
+  const searchParamas = new URLSearchParams();
+
+  for (const pair of formData) {
+    searchParamas.append(pair[0], pair[1]);
+  }
+
+  fetch("login.php", {
+    method: "post",
+    body: formData,
+  })
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (text) {
+      console.log(text);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
