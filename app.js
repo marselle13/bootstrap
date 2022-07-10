ce06879593cbacb3b77aec6c5f815f2dcb8c2346
marselle13 <98524205+marselle13@users.noreply.github.com>
2022-07-10 23:26:45 +0400

    final Commit

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 "use strict";
+//page reload data stay
+window.onload = function () {
+  let nameS = localStorage.getItem("name");
+  if (nameS !== null) $("#name").val(nameS);
 
-// Get started button
-$(".start-btn").on("click", function () {
-  location.href = "create.html";
-});
+  let emailS = localStorage.getItem("email");
+  if (emailS !== null) $("#email").val(emailS);
 
-//second page back button
-$(".back-btn").on("click", function () {
-  location.href = "index.html";
-});
-// third page back button
-$(".back-btn-third").on("click", function () {
-  location.href = "create.html";
-});
-// box number 1
-$("input").on("click", function () {
-  $(".box-1").css("background", "#E9FAF1");
-  $(".box-1").css("border", "none");
-});
+  let phoneS = localStorage.getItem("phone");
+  if (phoneS !== null) $("#phone").val(phoneS);
+
+  let dateS = localStorage.getItem("date_of_birth");
+  if (dateS !== null) $("#date").val(dateS);
+
+  let expS = localStorage.getItem("experience_level");
+  if (expS !== null) $("#exp").val(expS);
+
+  let charS = localStorage.getItem("character");
+  if (charS !== null) $("#char").val(charS);
+
+  let partS = localStorage.getItem("already_participated");
+  if (partS == true) $("#yes").checked == true;
+};
 
 const email = document.querySelector("#email");
 const names = document.querySelector("#name");
@@ -33,7 +37,26 @@ const error3 = document.querySelector(".error3-box");
 const error4 = document.querySelector(".error4-box");
 
 let regExp = /@redberry\.ge/;
+let phoneno = /^\d{9}$/;
+// Get started button
+$(".start-btn").on("click", function () {
+  location.href = "create.html";
+});
+// second page back button
+$(".back-btn").on("click", function () {
+  location.href = "index.html";
+});
+//third page back button
+$(".back-btn-third").on("click", function () {
+  location.href = "create.html";
+});
+// box number 1
+$("input").on("click", function () {
+  $(".box-1").css("background", "#E9FAF1");
+  $(".box-1").css("border", "none");
+});
 
+//checking inputs
 $("#submit").on("click", function (e) {
   e.preventDefault();
   check1();
@@ -44,7 +67,8 @@ $("#submit").on("click", function (e) {
   if (
     checker1.style.display === "block" &&
     checker2.style.display === "block" &&
-    checker3.style.display === "block"
+    checker3.style.display === "block" &&
+    checker4.style.display === "block"
   ) {
     $(".box-num1").text("");
     $(".box-num1").addClass("done");
@@ -52,12 +76,14 @@ $("#submit").on("click", function (e) {
   }
 });
 
+//name input check
 function check1() {
   // name checker
   if (names.value.length > 1) {
     checker1.style.display = "block";
     error1.style.display = "none";
     $("#name").css("background", "#fff");
+    localStorage.setItem("name", $("#name").val());
   } else {
     checker1.style.display = "none";
     error1.style.display = "block";
@@ -70,13 +96,14 @@ function check1() {
     $("#name").css("background", "#Fff");
   }
 }
-
+//email input check
 function check2() {
   // email checker
   if (email.value.match(regExp)) {
     checker2.style.display = "block";
     error2.style.display = "none";
     $("#email").css("background", "#Fff");
+    localStorage.setItem("email", $("#email").val());
   } else {
     checker2.style.display = "none";
     error2.style.display = "block";
@@ -89,12 +116,14 @@ function check2() {
   }
 }
 
+//phone input check
 function check3() {
   // phone checker
-  if (phone.value.length === 9) {
+  if (phone.value.match(phoneno)) {
     checker3.style.display = "block";
     error3.style.display = "none";
     $("#phone").css("background", "#Fff");
+    localStorage.setItem("phone", $("#phone").val());
   } else {
     checker3.style.display = "none";
     error3.style.display = "block";
@@ -106,14 +135,22 @@ function check3() {
     $("#phone").css("background", "#Fff");
   }
 }
-
+//date input check
 function check4() {
-  if (date.getAttribute("required") === "") {
-    checker4.style.display = "block";
+  if (checker3.style.display === "none") {
+    checker4.style.display = "none";
     error4.style.display = "none";
+    $("#date").css("background", "#Fff");
+  } else if (date.value) {
+    checker4.style.display = "block";
+    localStorage.setItem("date_of_birth", $("#date").val());
+    error4.style.display = "none";
+  } else if (!date.value) {
+    error4.style.display = "block";
+    $("#date").css("background", "#FFEFEF");
   }
 }
-
+//close pop ups
 $(".close1").on("click", function () {
   error1.style.display = "none";
   $("#name").css("background", "#Fff");
@@ -129,34 +166,30 @@ $(".close3").on("click", function () {
   $("#phone").css("background", "#Fff");
 });
 
-// function formatPhoneNumber(value) {
-//   if (!value) return value;
-//   const phoneNumber = value.replace(/[^\d]/g, "");
-//   const phoneNumberLength = phoneNumber.length;
-//   if (phoneNumberLength < 4) return phoneNumber;
-//   if (phoneNumverLength < 7) {
-//     return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
-//   }
-//   return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
-//     3,
-//     6
-//   )}-${phoneNumber.slice(6, 9)}`;
-// }
-
-// function phoneNumberFormatter() {
-//   const inputField = document.getElementById("phone");
-//   const formattedInputValue = formatPhoneNumber(inputField.value);
-//   inputField.value = formattedInputValue;
-// }
+$(".close4").on("click", function () {
+  error4.style.display = "none";
+  $("#date").css("background", "#Fff");
+});
 
 //level of knowledge dropdown
 function show(anything) {
   document.querySelector(".textBox").value = anything;
+  localStorage.setItem("experience_level", $(".textBox").val());
 }
 
 //choose your character dropdown
 function show2(anything) {
   document.querySelector(".textBox2").value = anything;
+  localStorage.setItem("character", $(".textBox2").val());
+  if ($(".textBox2").val() === "Nona Gaphrindashvili") {
+    localStorage.setItem("character_id", 1);
+  } else if ($(".textBox2").val() === "Mikhail Tal") {
+    localStorage.setItem("character_id", 2);
+  } else if ($(".textBox2").val() === "Bobby Fisher") {
+    localStorage.setItem("character_id", 3);
+  } else if ($(".textBox2").val() === "Magnus Carlsen") {
+    localStorage.setItem("character_id", 4);
+  }
 }
 // dropdown open
 let dropdown = document.querySelector(".dropdown");
@@ -170,6 +203,17 @@ dropdown2.onclick = function () {
   dropdown2.classList.toggle("active");
   $(".box-2").addClass("reg");
 };
+var yes = true;
+var no = false;
+//question
+function see() {
+  if (document.getElementById("yes").checked == true) {
+    localStorage.setItem("already_participated", true);
+  }
+  if (document.getElementById("no").checked == true) {
+    localStorage.setItem("already_participated", false);
+  }
+}
 
 // Json data
 const staticUrl = "https://chess-tournament-api.devtest.ge/api/grandmasters";
@@ -195,27 +239,26 @@ $(".done-btn").on("click", function () {
   location.href = "done.html";
 });
 
-//save data
-$("#form").addEventListener("submit", function (e) {
-  e.prevenetDefault();
-  const formData = new FormData(this);
-  const searchParamas = new URLSearchParams();
-
-  for (const pair of formData) {
-    searchParamas.append(pair[0], pair[1]);
-  }
-
-  fetch("login.php", {
-    method: "post",
-    body: formData,
+fetch("https://chess-tournament-api.devtest.ge/api/register", {
+  method: "POST",
+  body: JSON.stringify({
+    name: localStorage.getItem("name"),
+    email: localStorage.getItem("email"),
+    phone: localStorage.getItem("phone"),
+    date_of_birth: localStorage.getItem("date_of_birth"),
+    experience_level: localStorage.getItem("experience_level"),
+    already_participated: localStorage.getItem("already_participated"),
+    character_id: localStorage.getItem("character_id"),
+  }),
+  headers: {
+    accept: "application/json",
+    "Content-Type": "application/json",
+  },
+})
+  .then(function (response) {
+    return response.text();
   })
-    .then(function (response) {
-      return response.text();
-    })
-    .then(function (text) {
-      console.log(text);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-});
+  .then(function (text) {
+    console.log(text);
+  })
+  .catch(function (error) {});
